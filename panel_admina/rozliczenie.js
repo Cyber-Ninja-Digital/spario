9a298d9e8decc74237357ca07193ad9cb2697258
Artem Ryzhkov <artemryzhkov@MacBook-Pro-Artem.local>
2023-10-11 04:44:42 +0200

    Auto-commit: Wed Oct 11 04:44:42 CEST 2023. Changed files: panel_admina/rozliczenie.js panel_admina/rozliczenie.min.js

--- panel_admina/rozliczenie.js
+++ panel_admina/rozliczenie.js
@@ -52,16 +52,18 @@ function updateWeekInfo(dateFrom, dateTo) {
     const weekNumberFrom = getWeekNumber(new Date(dateFrom));
     const weekNumberTo = getWeekNumber(new Date(dateTo));
     document.getElementById('week-info').textContent = 
-        `Displaying data for the week: ${dateFrom} to ${dateTo} (Week numbers: ${weekNumberFrom} to ${weekNumberTo})`;
+        `Wyświetlane dane za tydzień: ${dateFrom} do ${dateTo} (Numery tygodni: ${weekNumberFrom} do ${weekNumberTo})`;
 }
+
 
 function getWeekNumber(d) {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-    const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    const weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
     return weekNo;
 }
+
 function getLastWeekDates() {
     const now = new Date();
     const lastSunday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay() + 1); // +1 to start week from Monday
