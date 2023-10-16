9d63482f838661f62331842eec39453d4774a2d4
artemcyberninja <138408856+artemcyberninja@users.noreply.github.com>
2023-10-16 18:53:16 +0200

    Update rozliczenie.js

--- panel_admina/rozliczenie.js
+++ panel_admina/rozliczenie.js
@@ -201,7 +201,7 @@ function updateCurrentPage() {
 
     document.getElementById('current-page').textContent = currentPage;
 }
-function showSkeletonLoader(rows = 17, columns = 20) {
+function showSkeletonLoader(rows = 15, columns = 22) {
     const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
     tableBody.innerHTML = "";
     for (let i = 0; i < rows; i++) {
