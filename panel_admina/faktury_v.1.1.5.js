16d7c2f53984a5ce181d569f98d3dd3fb006ed5a
artemcyberninja <138408856+artemcyberninja@users.noreply.github.com>
2023-11-20 10:18:37 +0100

    Update and rename faktury_v.1.1.3.js to faktury_v.1.1.5.js

--- panel_admina/faktury_v.1.1.5.js
+++ panel_admina/faktury_v.1.1.5.js
@@ -1,10 +1,9 @@
- let rowsPerPage = 15;
+  let rowsPerPage = 15;
  let sortDirections = Array(18).fill(true);  // For 18 columns
  document.addEventListener('DOMContentLoaded', function () {
          setLastWeekDates();
  const [dateFrom, dateTo] = getLastWeekDates();
  document.getElementById('date-range').value = `${dateFrom} - ${dateTo}`;
-
      updateWeekInfo(dateFrom, dateTo);
      showSkeletonLoader();
      loadAndDisplayData(dateFrom, dateTo);
@@ -24,10 +23,8 @@
      });
          document.getElementById('rows-per-page').addEventListener('change', function() {
              console.log('Rows per page changed:', this.value);  // Добавьте эту строку
-
          rowsPerPage = parseInt(this.value);
          console.log('Updated rowsPerPage:', rowsPerPage);  // Добавьте эту строку
-
          paginateData(globalData);  // Перерисовка таблицы с новым значением rowsPerPage
          document.getElementById('total-pages').textContent = Math.ceil(Object.keys(globalData).length / rowsPerPage);
          // Обновление номера текущей страницы
@@ -41,10 +38,8 @@
      const dateRange = document.getElementById('date-range').value.split(' - ');
      const dateFrom = new Date(dateRange[0]);
      const dateTo = new Date(dateRange[1]);
-
      const filteredData = invoicesData.filter(invoice => {
          const invoiceDate = new Date(invoice.purchaseDate);
-
          return (
              (invoice.driverId.toLowerCase().includes(searchValue) ||
               invoice.invoiceNumber.toLowerCase().includes(searchValue) ||
@@ -55,7 +50,6 @@
              (invoiceDate >= dateFrom && invoiceDate <= dateTo)
          );
      });
-
      paginateData(filteredData);
      document.getElementById('total-pages').textContent = Math.ceil(filteredData.length / rowsPerPage);
      currentPage = 1;
@@ -64,7 +58,6 @@
  document.getElementById('status-select').addEventListener('change', filterData);
      document.getElementById('type-select').addEventListener('change', filterData);
      document.getElementById('date-range').addEventListener('change', filterData);
-
          // Event listeners for sorting table when column headers are clicked
          let headers = document.querySelectorAll("#data-table th");
          headers.forEach((header, index) => {
@@ -77,8 +70,6 @@
      document.getElementById('week-info').textContent = 
          `Wyświetlane dane za tydzień: ${dateFrom} do ${dateTo} (Numery tygodni: ${weekNumberFrom} do ${weekNumberTo})`;
  }
-
-
  function getWeekNumber(d) {
      d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
      d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
@@ -86,7 +77,6 @@
      var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
      return weekNo;
  }
-
  function getLastWeekDates() {
      const now = new Date();
      const lastSunday = new Date(now.getFullYear(), now.getMonth(), now.getDate() - now.getDay() + 1); // +1 to start week from Monday
@@ -94,12 +84,10 @@
      const formatDate = date => date.toISOString().split('T')[0];
      return [formatDate(lastMonday), formatDate(lastSunday)];
  }
-
  function updateWeekInfo(dateFrom, dateTo) {
      document.getElementById('week-info').textContent = `Displaying data for the week: ${dateFrom} to ${dateTo}`;
  }
  let invoicesData = [];  // Глобальный массив для хранения данных о фактурах
-
 function loadAndDisplayData(dateFrom, dateTo) {
     const apiUrl = `https://us-central1-ccmcolorpartner.cloudfunctions.net/getDriversInvoicesForDateRange?dateFrom=${dateFrom}&dateTo=${dateTo}`;
     
@@ -119,10 +107,6 @@ function loadAndDisplayData(dateFrom, dateTo) {
             console.error('Error fetching invoices data: ', error);
         });
 }
-
-
-
-
  function formatNumber(value) {
      // Пробуем преобразовать строку в число
      if (typeof value === 'string') {
@@ -131,7 +115,6 @@ function loadAndDisplayData(dateFrom, dateTo) {
              value = numValue;
          }
      }
-
      if (typeof value === 'number') {
          return value.toFixed(2);
      } else if (value === null || value === undefined || value === "") {
@@ -141,75 +124,80 @@ function loadAndDisplayData(dateFrom, dateTo) {
      }
  }
 
-
-let globalData; // Глобальная переменная для хранения данных
+ let globalData; // Глобальная переменная для хранения данных
 
  function displayInvoicesInTable(data) {
-    const table = document.getElementById('data-table');
-    if (!table) {
-        console.error('Table with id "data-table" not found');
-        return;
-    }
-    const tableBody = table.getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = "";
+     const table = document.getElementById('data-table');
+     if (!table) {
+         console.error('Table with id "data-table" not found');
+         return;
+     }
 
-    if (typeof data !== 'object' || data === null) {
-        console.error('Data is not a valid object:', data);
-        return;
-    }
+     // Проверка валидности данных
+     if (typeof data !== 'object' || data === null) {
+         console.error('Data is not a valid object:', data);
+         return;
+     }
 
-   for (const driverName in data) {
-        if (data.hasOwnProperty(driverName)) {
-            const invoices = data[driverName];
-            for (const invoiceId in invoices) {
-                if (invoices.hasOwnProperty(invoiceId)) {
+     // Преобразование данных в массив для удобства пагинации
+     const invoicesArray = [];
+     for (const driverName in data) {
+         if (data.hasOwnProperty(driverName)) {
+             const invoices = data[driverName];
+             for (const invoiceId in invoices) {
+                 if (invoices.hasOwnProperty(invoiceId)) {
                     const invoice = invoices[invoiceId];
-                    const row = tableBody.insertRow();
+                     const row = tableBody.insertRow();
 
-                    row.insertCell().appendChild(document.createTextNode(driverName || "N/A")); // Используйте driverName
-        row.insertCell().appendChild(document.createTextNode(invoice.numerfaktury || "N/A"));
-        row.insertCell().appendChild(document.createTextNode(invoice.purchaseDate || "N/A"));
-        row.insertCell().appendChild(document.createTextNode(invoice.type || "N/A"));
-        row.insertCell().appendChild(document.createTextNode(invoice.registrationNumber || "Nie dotyczy"));
-        row.insertCell().appendChild(document.createTextNode(invoice.nipseller || "N/A"));
-        row.insertCell().appendChild(document.createTextNode(invoice.liters || "0"));
-        row.insertCell().appendChild(document.createTextNode(invoice.fuelType || "N/A"));
-        row.insertCell().appendChild(document.createTextNode(invoice.grossAmount || "0"));
-        row.insertCell().appendChild(document.createTextNode(invoice.vatRate || "0%"));
-        row.insertCell().appendChild(document.createTextNode(invoice.netAmount || "0"));
-        row.insertCell().appendChild(document.createTextNode(invoice.vatAmount || "0"));
-        row.insertCell().appendChild(document.createTextNode(invoice.vatReturn || "0"));
-        row.insertCell().appendChild(document.createTextNode(invoice.status || "N/A"));
+                     row.insertCell().appendChild(document.createTextNode(driverName || "N/A")); // Используйте driverName
+         row.insertCell().appendChild(document.createTextNode(invoice.numerfaktury || "N/A"));
+         row.insertCell().appendChild(document.createTextNode(invoice.purchaseDate || "N/A"));
+         row.insertCell().appendChild(document.createTextNode(invoice.type || "N/A"));
+         row.insertCell().appendChild(document.createTextNode(invoice.registrationNumber || "Nie dotyczy"));
+         row.insertCell().appendChild(document.createTextNode(invoice.nipseller || "N/A"));
+         row.insertCell().appendChild(document.createTextNode(invoice.liters || "0"));
+         row.insertCell().appendChild(document.createTextNode(invoice.fuelType || "N/A"));
+         row.insertCell().appendChild(document.createTextNode(invoice.grossAmount || "0"));
+         row.insertCell().appendChild(document.createTextNode(invoice.vatRate || "0%"));
+         row.insertCell().appendChild(document.createTextNode(invoice.netAmount || "0"));
+         row.insertCell().appendChild(document.createTextNode(invoice.vatAmount || "0"));
+         row.insertCell().appendChild(document.createTextNode(invoice.vatReturn || "0"));
+         row.insertCell().appendChild(document.createTextNode(invoice.status || "N/A"));
 
-        // Для cellChangeStatus нужно создать элемент select или другой интерактивный элемент
-        const selectStatus = document.createElement('select');
-        // Заполнение select значениями статусов, можно добавить логику изменения статуса здесь
-        selectStatus.innerHTML = '<option value="zaakceptowany">zaakceptowany</option>';
-        selectStatus.value = invoice.status;
-        row.insertCell().appendChild(selectStatus);
+         // Для cellChangeStatus нужно создать элемент select или другой интерактивный элемент
+         const selectStatus = document.createElement('select');
+         // Заполнение select значениями статусов, можно добавить логику изменения статуса здесь
+         selectStatus.innerHTML = '<option value="zaakceptowany">zaakceptowany</option>';
+         selectStatus.value = invoice.status;
+         row.insertCell().appendChild(selectStatus);
 
-        // Для cellFilePreview можно добавить ссылку, если есть URL файла
-        const fileLink = document.createElement('a');
-        fileLink.href = invoice.fileURL || "#";
-        fileLink.textContent = invoice.fileURL ? "Zobacz plik" : "Nie ma pliku";
-        row.insertCell().appendChild(fileLink);
+         // Для cellFilePreview можно добавить ссылку, если есть URL файла
+         const fileLink = document.createElement('a');
+         fileLink.href = invoice.fileURL || "#";
+         fileLink.textContent = invoice.fileURL ? "Zobacz plik" : "Nie ma pliku";
+         row.insertCell().appendChild(fileLink);
 
-        row.insertCell().appendChild(document.createTextNode(invoice.rejectionComment || "N/A"));
-        row.insertCell().appendChild(document.createTextNode(invoice.statusSprawdzenia || "N/A"));
-                }
-            }
-        }
-    }
-        document.getElementById('total-pages').textContent = Math.ceil(Object.keys(data).length / rowsPerPage);
-    paginateData(data);
-}
+         row.insertCell().appendChild(document.createTextNode(invoice.rejectionComment || "N/A"));
+         row.insertCell().appendChild(document.createTextNode(invoice.statusSprawdzenia || "N/A"));                 }
+             }
+         }
+     }
 
- let currentPage = 1;
+     // Сохраняем данные в глобальную переменную
+     globalData = invoicesArray;
+
+     // Обновляем общее количество страниц
+     document.getElementById('total-pages').textContent = Math.ceil(invoicesArray.length / rowsPerPage);
+
+     // Вызываем пагинацию для отображения первой страницы данных
+     paginateData(invoicesArray);
+ }
+
+  let currentPage = 1;
  function paginateData(data) {
-     console.log('Paginating data with', rowsPerPage, 'rows per page');
-
-     const tableBody = document.getElementById('invoices-table').getElementsByTagName('tbody')[0];
+     const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
      tableBody.innerHTML = "";
+
      const startIndex = (currentPage - 1) * rowsPerPage;
      const endIndex = startIndex + rowsPerPage;
      const paginatedData = data.slice(startIndex, endIndex);
@@ -217,8 +205,8 @@ let globalData; // Глобальная переменная для хранен
      for (const invoice of paginatedData) {
          const row = tableBody.insertRow();
 
-         const cellDriverId = row.insertCell();
-         const cellInvoiceNumber = row.insertCell();
+          const cellDriverId = row.insertCell();
+          const cellInvoiceNumber = row.insertCell();
          const cellPurchaseDate = row.insertCell();
          const cellType = row.insertCell();
          const cellRegistrationNumber = row.insertCell();
@@ -235,7 +223,6 @@ let globalData; // Глобальная переменная для хранен
          const cellFilePreview = row.insertCell();
          const cellRejectionComment = row.insertCell();
          const cellstatusSprawdzenia = row.insertCell();
-
          cellDriverId.textContent = invoice.driverId;
          cellInvoiceNumber.textContent = invoice.numerfaktury;
          cellPurchaseDate.textContent = invoice.purchaseDate;
@@ -253,7 +240,6 @@ let globalData; // Глобальная переменная для хранен
          cellFilePreview.innerHTML = invoice.fileURL ? '<a href="' + invoice.fileURL + '" target="_blank">Zobacz plik</a>' : 'Nie ma pliku';
          cellRejectionComment.textContent = invoice.rejectionComment;
          cellstatusSprawdzenia.textContent = invoice.statusSprawdzenia;
-
          // Пример создания выпадающего списка для изменения статуса
          const select = document.createElement('select');
          select.innerHTML = `
@@ -284,7 +270,6 @@ let globalData; // Глобальная переменная для хранен
  }
  function updateCurrentPage() {
      console.log('Updating current page to', currentPage);  // Добавьте эту строку
-
      document.getElementById('current-page').textContent = currentPage;
  }
  function showSkeletonLoader(rows = 15, columns = 18) {
@@ -302,29 +287,23 @@ let globalData; // Глобальная переменная для хранен
          }
      }
  }
-
  function sortTable(columnIndex) {
      const tableBody = document.getElementById('data-table').getElementsByTagName('tbody')[0];
      let rows = Array.from(tableBody.getElementsByTagName('tr'));
      const isAscending = sortDirections[columnIndex];
-
      rows.sort((a, b) => {
          let cellA = a.cells[columnIndex].innerText;
          let cellB = b.cells[columnIndex].innerText;
-
          if (!isNaN(cellA) && !isNaN(cellB)) {  // If it's a number
              cellA = parseFloat(cellA);
              cellB = parseFloat(cellB);
          }
-
          if (cellA < cellB) return isAscending ? -1 : 1;
          if (cellA > cellB) return isAscending ? 1 : -1;
          return 0;
      });
-
      // Update the direction for the next time the column is clicked
      sortDirections[columnIndex] = !isAscending;
-
      // Append the sorted rows to the table body
      tableBody.innerHTML = "";
      for (let row of rows) {
@@ -345,12 +324,10 @@ let globalData; // Глобальная переменная для хранен
          "opens": "center"
      });
  });
-
  document.getElementById('load-data').addEventListener('click', function() {
      const dateInputs = document.querySelector('#date-control input[type="text"]').value.split(' - ');
      const dateFrom = dateInputs[0];
      const dateTo = dateInputs[1];
-
      updateWeekInfo(dateFrom, dateTo);
      showSkeletonLoader();
      loadAndDisplayData(dateFrom, dateTo);
