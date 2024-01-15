6484ea04d20e67e0381df71238e1398ea6f809fa
artemcyberninja <138408856+artemcyberninja@users.noreply.github.com>
2024-01-15 10:57:12 +0100

    Update and rename rozliczenie_v0.1.37.js to rozliczenie_v0.1.38.js

--- panel_admina/rozliczenie_v0.1.38.js
+++ panel_admina/rozliczenie_v0.1.38.js
@@ -71,9 +71,10 @@ function getWeekNumber(d) {
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
-    var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
-    return weekNo;
+var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+return ${weekNo}-${d.getUTCFullYear()}; // Возвращает номер недели и год
 }
+
 
 function getLastWeekDates() {
     const now = new Date();
